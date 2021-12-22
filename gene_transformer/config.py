406ab5a87b66ec3180c194ae4059bd6f44498959
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-12-22 14:18:12 -0800

    fixing config

--- gene_transformer/config.py
+++ gene_transformer/config.py
@@ -1,4 +1,21 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings as _BaseSettings
+from pathlib import Path
+from typing import TypeVar, Type, Union, Optional, Dict, Any
+import yaml
+import json
+
+_T = TypeVar("_T")
+
+class BaseSettings(_BaseSettings):
+    def dump_yaml(self, cfg_path):
+        with open(cfg_path, mode="w") as fp:
+            yaml.dump(json.loads(self.json()), fp, indent=4, sort_keys=False)
+
+    @classmethod
+    def from_yaml(cls: Type[_T], filename: Union[str, Path]) -> _T:
+        with open(filename) as fp:
+            raw_data = yaml.safe_load(fp)
+        return cls(**raw_data)
 
 class ModelSettings(BaseSettings):
     # logging settings
@@ -20,4 +37,4 @@ class ModelSettings(BaseSettings):
 
 if __name__ == "__main__":
     settings = ModelSettings()
-    settings.yaml("settings_template.yaml")
+    settings.dump_yaml("settings_template.yaml")
