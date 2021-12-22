9171847cd3a433ee0ca0b9d287bca6e6e337da42
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-12-22 15:57:16 -0800

    Update model.py

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -46,7 +46,6 @@ class DNATransform(pl.LightningModule):
         x = batch
         outputs = self(x)
         loss = outputs.losses.mean()
-        pdb.set_trace()
         score = outputs.prediction_scores.mean()
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("train_prediction_score", score, on_step=True, on_epoch=True, logger=True)
@@ -56,6 +55,7 @@ class DNATransform(pl.LightningModule):
         x = batch
         outputs = self(x)
         loss = outputs.losses.mean()
+        pdb.set_trace()
         score = outputs.prediction_scores.mean()
         self.log("validation_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("validation_prediction_score", score, on_step=True, on_epoch=True, logger=True)
