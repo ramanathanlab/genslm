0319a7a8d35d1faa60b1069b58a9cfc393c13979
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-01-05 20:08:55 -0800

    back to dp

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     else:
         wandb_logger = None
     checkpoint_callback = ModelCheckpoint(monitor="train_loss", every_n_train_steps=config.checkpoint_interval)
-    trainer = pl.Trainer(gpus=-1, default_root_dir=config.checkpoint_dir, strategy="ddp_spawn",
+    trainer = pl.Trainer(gpus=-1, default_root_dir=config.checkpoint_dir, strategy="dp",
                          callbacks=[checkpoint_callback], max_epochs=config.epochs, logger=wandb_logger)
     trainer.fit(model)
     print("Completed training.")
