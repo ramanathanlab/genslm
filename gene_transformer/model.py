c9e37bc801566aafd0abc1cf3c9f6150d8ec1d7e
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-01-26 17:04:24 -0700

    16bit preciion

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
         val_check_interval=config.val_check_interval,
         accumulate_grad_batches=config.accumulate_grad_batches,
         num_sanity_val_steps=2,
-        # precision=16
+        precision=16
     )
     trainer.fit(model)
     trainer.test(model)
