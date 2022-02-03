eda854295bb734f4fb4a8034cc2472bf374a2873
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-02-03 12:53:32 -0700

    Update model.py

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -279,7 +279,7 @@ if __name__ == "__main__":
             offload_optimizer=True,
             offload_parameters=False,
             remote_device="nvme",
-            offload_params_device="nvme",
+            # offload_params_device="nvme",
             offload_optimizer_device="nvme",
             nvme_path=os.environ['PSCRATCH']
         ),
