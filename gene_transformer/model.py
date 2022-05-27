ee3332f42bbc4cf2e45d4281b17b09f8068c479c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-05-27 15:22:17 -0600

    axial pos embed

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -65,6 +65,7 @@ class DNATransformer(pl.LightningModule):
             self.cfg.model_name,
             vocab_size=self.tokenizer.vocab_size,
             feed_forward_size=self.cfg.block_size,
+            axial_pos_embds=False
             # max_position_embeddings=self.cfg.block_size,
         )
         self.model = AutoModelForCausalLM.from_config(base_config)
