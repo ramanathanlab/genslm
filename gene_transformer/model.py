cfe3fcb636f500164706ebf4a4201479013f66c3
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-05-31 17:00:52 -0600

    Update model.py

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -68,7 +68,8 @@ class DNATransformer(pl.LightningModule):
             # axial_pos_embds=False,
             local_chunk_length=100,
             lsh_attn_chunk_length=100,
-            axial_pos_shape=(102, 118)
+            axial_pos_shape=(102, 118),
+            max_position_embeddings=cfg.block_size,
             # max_position_embeddings=self.cfg.block_size,
         )
         self.model = AutoModelForCausalLM.from_config(base_config)
