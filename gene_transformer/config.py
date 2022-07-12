7ebde63e606765f47fc4867baae1859d04ec06dc
braceal <braceal@umich.edu>
2022-07-12 13:48:03 -0500

    docstrings for config

--- gene_transformer/config.py
+++ gene_transformer/config.py
@@ -1,3 +1,4 @@
+"""Model configuration."""
 import json
 from pathlib import Path
 from typing import Optional, Type, TypeVar, Union
@@ -11,6 +12,8 @@ PathLike = Union[str, Path]
 
 
 class BaseSettings(_BaseSettings):
+    """Base settings to provide an easier interface to read/write YAML files."""
+
     def dump_yaml(self, cfg_path: PathLike) -> None:
         with open(cfg_path, mode="w") as fp:
             yaml.dump(json.loads(self.json()), fp, indent=4, sort_keys=False)
@@ -32,49 +35,83 @@ class WarmupLRSettings(BaseSettings):
 
 
 class ModelSettings(BaseSettings):
+    """Settings for the DNATransformer model."""
+
     # logging settings
     wandb_active: bool = True
+    """Whether to use wandb for logging."""
     wandb_project_name: str = "codon_transformer"
+    """Wandb project name to log to."""
     checkpoint_dir: Path = Path("codon_transformer")
+    """Checkpoint directory to backup model weights."""
     node_local_path: Optional[Path] = None
+    """A node local storage option to write temporary files to."""
     num_nodes: int = 1
+    """The number of compute nodes used for training."""
 
     # data settings
-    genome_level: bool = False
     tokenizer_file: Path = Path("tokenizer_files/codon_wordlevel_100vocab.json")
+    """Path to the tokenizer file."""
     train_file: Path
+    """Path to the training data."""
     val_file: Path
+    """Path to the validation data."""
     test_file: Path
+    """Path to the testing data."""
     kmer_size: int = 3
+    """Size of kmer to use for tokenization."""
+    genome_level: bool = False
+    """Whether or not to use the genome-scale dataset class."""
     small_subset: int = 0
+    """Only applies when :obj:`genome_level` is true. Uses the full dataset by default."""
 
     # blast settings
     enable_blast: bool = True
+    """Whether or not to run BLAST during validation steps."""
     blast_validation_file: Path = Path("blast_file.fasta")
+    """Path to fasta file to BLAST against."""
     num_blast_seqs_per_gpu: int = 5
-    blast_exe_path: Path = Path("blastn")  # Defaults to current conda environment
+    """Number of BLAST jobs per GPU/rank."""
+    blast_exe_path: Path = Path("blastn")
+    """Path to the BLAST executable, defaults to current conda environment."""
 
     # model settings
     model_name: str = "gpt2"
+    """Name of the huggingface model to use."""
     batch_size: int = 8
+    """Training micro-batch size."""
     epochs: int = 5
+    """Number of training epochs."""
     block_size: int = 512
+    """Block size to specify sequence length passed to the transformer."""
     accumulate_grad_batches: int = 4
+    """Number of batches to accumulate before gradient updates."""
     learning_rate: float = 5e-5
+    """Learning rate to use for training."""
     precision: int = 16
+    """Training precision."""
     warm_up_lr: Optional[WarmupLRSettings] = None
+    """If specified, will use a learning rate warmup scheduler."""
     load_from_checkpoint_dir: Optional[Path] = None
+    """If specified, will load a model weight checkpoint to resume training from."""
     deepspeed_cfg_file: Optional[Path] = None
+    """The deepspeed configuration file (currently unused)."""
 
     # generation settings
     num_test_seqs_per_gpu: int = 8
+    """Number of sequences to generate per GPU when testing."""
     custom_seq_name: str = "SyntheticSeq"
+    """Custum sequence name to write into fasta files for generate sequences."""
 
-    # training ops
+    # training ops (see PyTorch DataLoader for details.)
     num_data_workers: int = 4
+    """Number of subprocesses to use for data loading."""
     prefetch_factor: int = 4
+    """Number of batches loaded in advance by each worker."""
     pin_memory: bool = True
+    """If True, the data loader will copy Tensors into device/CUDA pinned memory before returning them."""
     persistent_workers: bool = True
+    """If True, the data loader will not shutdown the worker processes after a dataset has been consumed once."""
 
 
 if __name__ == "__main__":
