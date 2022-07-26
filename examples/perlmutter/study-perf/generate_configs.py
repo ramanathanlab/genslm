a23238a78585907ed4b28cae9e989c7034d64a48
braceal <braceal@umich.edu>
2022-07-25 19:23:26 -0500

    generation script

--- examples/perlmutter/study-perf/generate_configs.py
+++ examples/perlmutter/study-perf/generate_configs.py
@@ -10,24 +10,41 @@ def parse_args():
         "--config_dir",
         type=Path,
         required=True,
-        help="Directory to write config files to",
+        help="Directory to write config files to.",
     )
     parser.add_argument("--checkpoint_dir", type=Path, required=True)
     parser.add_argument("--tokenizer_file", type=Path, required=True)
+    parser.add_argument("--train_file", type=Path, required=True)
+    parser.add_argument("--val_file", type=Path, required=True)
+    parser.add_argument("--test_file", type=Path, required=True)
     args = parser.parse_args()
     return args
 
 
-if __name__ == "__main__":
-
+def main() -> None:
     args = parse_args()
     args.config_dir.mkdir(exist_ok=True)
     args.checkpoint_dir.mkdir(exist_ok=True)
 
-    model_names = ["gpt2", "reformer", "gpt-neox"]
+    model_names = ["reformer", "gpt-neox"]
     num_nodes = [1, 2, 4, 8, 16, 32, 64, 128, 256]
     num_params = ["25M", "250M", "2.5B", "20B"]
-    block_sizes = ["protein-scale", "genome-scale"]
+    block_sizes = [512, 10240]  # protein vs genome scale
+
+    model_architectures = {
+        "reformer": {
+            "25M": "/path/to/model.json",
+            "250M": "/path/to/model.json",
+            "2.5B": "/path/to/model.json",
+            "20B": "/path/to/model.json",
+        },
+        "gpt-neox": {
+            "25M": "/path/to/model.json",
+            "250M": "/path/to/model.json",
+            "2.5B": "/path/to/model.json",
+            "20B": "/path/to/model.json",
+        },
+    }
 
     experiment_combinations = list(
         product(model_names, num_nodes, num_params, block_sizes)
@@ -36,14 +53,25 @@ if __name__ == "__main__":
         model_name, nodes, params, block_size = experiment
         experiment_name = f"{model_name}_{nodes}nodes_{params}_{block_size}"
         print(experiment_name)
-        # TODO: Add more arguments, translate experiment params into valid model architectures.
         config = ModelSettings(
             checkpoint_dir=args.checkpoint_dir,
-            node_local_path="/tmp",
+            node_local_path=Path("/tmp"),
             num_nodes=nodes,
             compute_throughput=True,
             tokenizer_file=args.tokenizer_file,
-            ...
+            train_file=args.train_file,
+            val_file=args.val_file,
+            test_file=args.test_file,
+            genome_level=(block_size == 10240),
+            model_config_json=Path(model_architectures[model_name][params]),
+            batch_size=4,
+            block_size=block_size,
         )
         config_path = args.config_dir / f"{experiment_name}.yaml"
-        config.write_yaml(config_path)
+        config.dump_yaml(config_path)
+
+
+if __name__ == "__main__":
+    main()
+    # TODO: Set automatically with lightning?
+    # TODO: Set json files to specify model architectures
