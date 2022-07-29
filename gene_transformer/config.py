1d7d13c305754f19cc79fc9fc5ff70f42d79385b
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-07-28 21:08:13 -0600

    add analysis notebook, updated compute throughput

--- gene_transformer/config.py
+++ gene_transformer/config.py
@@ -145,7 +145,10 @@ def throughput_config(cfg: ModelSettings) -> ModelSettings:
     new_config.epochs = 6
     new_config.check_val_every_n_epoch = 7
     new_config.num_test_seqs_per_gpu = 0
-    new_config.small_subset = 16000
+    if cfg.num_nodes == 1:
+        new_config.small_subset = 1600
+    else:
+        new_config.small_subset = 16000
     new_config.profiling_path = None
     return new_config
 
