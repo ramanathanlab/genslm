49bb420a7a1f8d022c6eee59b7d4df6abc6ad269
braceal <braceal@umich.edu>
2022-08-18 14:38:21 -0500

    docs

--- data_processing/run_mmseqs.py
+++ data_processing/run_mmseqs.py
@@ -3,19 +3,33 @@ from pathlib import Path
 from argparse import ArgumentParser
 
 
-def main(args):
-    # make the outdir
-    args.out_dir.mkdir(exist_ok=True)
+def mmseqs2_easy_cluster(
+    fasta: Path, output_dir: Path, similarity: float, mmseqs_exe: str = "mmseqs"
+):
+    """Run easy-cluster mmseqs2 executable and return the number of non redundant sequences.
 
-    out_dir_and_files = args.out_dir / f"{args.out_ext}_sim{args.sim}"
-    temp_dir = args.out_dir / "temp"
-    command = f"{args.mmseqs} easy-cluster {args.fasta} {out_dir_and_files} {temp_dir} --min-seq-id {args.sim}"
+    Parameters
+    ----------
+    fasta : Path
+        Path to fasta file.
+    output_dir : Path
+        Output directory for mmseqs2 executable.
+    similarity : float
+        min-seq-id similarity for mmseqs2.
+    mmseqs_exe : str
+        mmseqs executable.
+    """
+    output_dir.mkdir(exist_ok=True)
+
+    out_dir_and_files = output_dir / f"sim{similarity}"
+    temp_dir = output_dir / "temp"
+    command = f"{mmseqs_exe} easy-cluster {fasta} {out_dir_and_files} {temp_dir} --min-seq-id {similarity}"
 
     proc = subprocess.run(command.split())
 
     if proc.returncode == 0:
         print("\n\n")
-        print(f"Succesfully clustered input fasta file to: {args.out_dir}")
+        print(f"Succesfully clustered input fasta file to: {output_dir}")
     else:
         print("\n\n")
         print("MMSEQS did not sucessfully complete, see above output")
@@ -23,17 +37,30 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--fasta", type=Path, required=True, help="Path to the fasta file input")
     parser.add_argument(
-        "--out_dir", type=Path, required=True, help="Path to the output directory, will be made if it does not exist"
+        "--fasta", type=Path, required=True, help="Path to the fasta file input"
     )
     parser.add_argument(
-        "--out_ext", type=str, help="Extension to give the output files names, defaults to 'res'", default="res"
+        "--out_dir",
+        type=Path,
+        required=True,
+        help="Path to the output directory, will be made if it does not exist",
     )
     parser.add_argument(
-        "--mmseqs", type=str, help="Path to MMSEQS program", default="/home/kyle/anaconda3/envs/gat_go/bin/mmseqs"
+        "--out_ext",
+        type=str,
+        help="Extension to give the output files names, defaults to 'res'",
+        default="res",
     )
-    parser.add_argument("--sim", type=float, default=0.5, help="Similarity threshold to run mmseqs with")
+    parser.add_argument(
+        "--mmseqs",
+        type=str,
+        help="Path to MMSEQS program",
+        default="/home/kyle/anaconda3/envs/gat_go/bin/mmseqs",
+    )
+    parser.add_argument(
+        "--sim", type=float, default=0.5, help="Similarity threshold to run mmseqs with"
+    )
 
     args = parser.parse_args()
     main(args)
