90d1dd1ecc4198e5c5b333aead7e3739441da3d7
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-08-29 14:55:37 -0600

    debugging

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -141,6 +141,7 @@ class DNATransformer(pl.LightningModule):
             labels=batch["input_ids"],
             attention_mask=batch["attention_mask"],
             output_hidden_states=True)
+        pdb.set_trace()
         # outputs.hidden_states: (batch_size, sequence_length, hidden_size)
         emb = outputs.hidden_states[0].detach().cpu().numpy()
         # if compute_mean:
