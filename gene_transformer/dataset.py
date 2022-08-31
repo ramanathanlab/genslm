7ef8c50d73077f590652e3f3dc5d10f1c801382d
Kyle Hippe <kyle.hippe@gmail.com>
2022-08-31 13:22:26 -0500

    Testing vlen strinsg

--- gene_transformer/dataset.py
+++ gene_transformer/dataset.py
@@ -160,12 +160,10 @@ class H5Dataset(Dataset):
         for key in ["id", "description", "sequence"]:
             fields[key] = ragged_array(fields[key])
 
-        exit()
-
-        # TODO: Some of these arrays (id, description, attention_mask) may be ragged
-
         # Write to HDF5 file
         with h5py.File(output_file, "w") as f:
+            str_dtype = h5py.vlen_dtype(h5py.string_dtype(encoding="utf-8"))
+
             # TODO: Experiment with smaller compression ratio
             create_dataset = functools.partial(
                 f.create_dataset,
@@ -178,6 +176,6 @@ class H5Dataset(Dataset):
             # TODO: Which is the best type to use?
             create_dataset("attention_mask", data=fields["attention_mask"], dtype="i8")
             # TODO: Test/debug: https://docs.h5py.org/en/stable/strings.html
-            create_dataset("id", data=fields["id"], dtype="S")
-            create_dataset("description", data=fields["description"], dtype="S")
-            create_dataset("sequence", data=fields["sequence"], dtype="S")
+            create_dataset("id", data=fields["id"], dtype=str_dtype)
+            create_dataset("description", data=fields["description"], dtype=str_dtype)
+            create_dataset("sequence", data=fields["sequence"], dtype=str_dtype)
