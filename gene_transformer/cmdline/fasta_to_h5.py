e27cfb3e707730065dfb7792ddbd62257d09d8e6
Kyle Hippe <kyle.hippe@gmail.com>
2022-08-31 16:01:08 -0500

    Trying to gather now

--- gene_transformer/cmdline/fasta_to_h5.py
+++ gene_transformer/cmdline/fasta_to_h5.py
@@ -29,7 +29,7 @@ def process_dataset(
             raise ValueError("H5 in directory not present")
 
         print("Gathering...")
-        # H5Dataset.gather(h5_dir, h5_outfile)
+        H5Dataset.gather(h5_dir, h5_outfile)
         exit()
 
     if not fasta_dir:
@@ -40,7 +40,9 @@ def process_dataset(
         raise ValueError("Output dir not present")
 
     output_dir.mkdir(exist_ok=True)
-    tokenizer = PreTrainedTokenizerFast(tokenizer_object=Tokenizer.from_file(str(tokenizer_file)))
+    tokenizer = PreTrainedTokenizerFast(
+        tokenizer_object=Tokenizer.from_file(str(tokenizer_file))
+    )
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
     files = list(fasta_dir.glob(glob_pattern))
     out_files = [output_dir / f"{f.stem}.h5" for f in files]
@@ -49,10 +51,18 @@ def process_dataset(
     if len(already_done) == len(files):
         raise ValueError(f"Already processed all files in {fasta_dir}")
 
-    files, out_files = zip(*[(fin, fout) for fin, fout in zip(files, out_files) if fout.name not in already_done])
+    files, out_files = zip(
+        *[
+            (fin, fout)
+            for fin, fout in zip(files, out_files)
+            if fout.name not in already_done
+        ]
+    )
 
     print(f"Processing {len(files)} files from {fasta_dir}...")
-    func = functools.partial(H5Dataset.preprocess, tokenizer=tokenizer, block_size=tokenizer_blocksize)
+    func = functools.partial(
+        H5Dataset.preprocess, tokenizer=tokenizer, block_size=tokenizer_blocksize
+    )
 
     with ProcessPoolExecutor(max_workers=num_workers) as pool:
         for _ in pool.map(func, files, out_files):
