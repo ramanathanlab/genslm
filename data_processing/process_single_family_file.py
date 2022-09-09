f8ad28a6229f8520677df7c1b6f62b504478eaaf
Kyle Hippe <kyle.hippe@gmail.com>
2022-09-09 13:32:58 -0500

    Trying to auto resolve path to tokenizer file

--- data_processing/process_single_family_file.py
+++ data_processing/process_single_family_file.py
@@ -18,7 +18,9 @@ def main(
     compression_ratio: int,
 ):
     print(tokenizer_path)
-    tokenizer = PreTrainedTokenizerFast(tokenizer_object=Tokenizer.from_file(str(tokenizer_path)))
+    tokenizer = PreTrainedTokenizerFast(
+        tokenizer_object=Tokenizer.from_file(str(tokenizer_path))
+    )
 
     H5Dataset.preprocess(
         input,
@@ -33,16 +35,27 @@ def main(
 if __name__ == "__main__":
     fp = Path(__file__)
     parser = ArgumentParser()
-    parser.add_argument("-i", "--input", help="Path to input file", required=True, type=Path),
-    parser.add_argument("-o", "--output", help="Path to output h5 file", required=True, type=Path)
+    parser.add_argument(
+        "-i", "--input", help="Path to input file", required=True, type=Path
+    ),
+    parser.add_argument(
+        "-o", "--output", help="Path to output h5 file", required=True, type=Path
+    )
     parser.add_argument(
         "-t",
         "--tokenizer_file",
         help="Path to tokenizer file",
-        default=(fp.parent.parent.resolve() / "gene_transformer/tokenizer_files/codon_wordlevel_100vocab.json"),
+        default=(
+            fp.parent.resolve()
+            / "gene_transformer/tokenizer_files/codon_wordlevel_100vocab.json"
+        ),
     )
     parser.add_argument(
-        "-cr", "--compression_ratio", help="Compression ratio to use for the H5 file, (0-9)", type=int, default=6
+        "-cr",
+        "--compression_ratio",
+        help="Compression ratio to use for the H5 file, (0-9)",
+        type=int,
+        default=6,
     )
     parser.add_argument(
         "-ct",
@@ -60,4 +73,11 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    main(args.input, args.output, args.tokenizer_file, args.block_size, args.compression_ratio, args.compression_type)
+    main(
+        args.input,
+        args.output,
+        args.tokenizer_file,
+        args.block_size,
+        args.compression_ratio,
+        args.compression_type,
+    )
