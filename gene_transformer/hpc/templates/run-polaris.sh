b969fe44d503c681ca40128805499d1eef2d86cc
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-09-09 09:40:48 -0600

    Update run-polaris.sh

--- gene_transformer/hpc/templates/run-polaris.sh
+++ gene_transformer/hpc/templates/run-polaris.sh
@@ -6,6 +6,7 @@ export RANK=$((${PMI_RANK} % ${NRANKS})) # Experimental wandb fix
 # perform logic check to find rank zero - if rank is zero and local rank is none
 if [ $RANK == 0 ] && [ -z "$LOCAL_RANK" ]
 then
+  echo "FOUND RANK ZERO"
   export WANDB_ACTIVE=1
 else
   export WANDB_ACTIVE=0
