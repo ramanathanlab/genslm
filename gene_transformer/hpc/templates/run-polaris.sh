00572a4b3c4e239c29ced22d830b32f7fdc231d8
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-09-09 11:13:39 -0600

    remove commented out logic from script

--- gene_transformer/hpc/templates/run-polaris.sh
+++ gene_transformer/hpc/templates/run-polaris.sh
@@ -3,12 +3,4 @@ export NRANKS=$(wc -l <"${PBS_NODEFILE}")
 export NODE_RANK=$((${PMI_RANK} % ${NRANKS}))
 export GLOBAL_RANK=$((${PMI_RANK} % ${NRANKS}))
 export RANK=$((${PMI_RANK} % ${NRANKS})) # Experimental wandb fix
-# perform logic check to find rank zero - if rank is zero and local rank is none
-#if [ $RANK == 0 ] && [ -z "$LOCAL_RANK" ]
-#then
-#  echo "FOUND RANK ZERO"
-#  export WANDB_ACTIVE=1
-#else
-#  export WANDB_ACTIVE=0
-#fi
 exec "$@"
