4eac1c9cf194fed9ed76c6f4f812f75af9a2b7d8
braceal <braceal@umich.edu>
2022-09-09 23:02:13 -0500

    h5 to fasta

--- gene_transformer/cmdline/h5_to_fasta.py
+++ gene_transformer/cmdline/h5_to_fasta.py
@@ -1,3 +1,4 @@
+"""Example usage: python -m gene_transformer.cmdline.h5_to_fasta -i /path/to/h5_dir -o ouput.fasta -w 32 -s 250"""
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -7,8 +8,8 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-i", "--h5_dir", type=str, help="Directory with *.h5 files")
     parser.add_argument("-o", "--output_file", type=Path, help="fasta file.")
-    parser.add_argument("-n", "--num_workers", type=int, default=1)
-    parser.add_argument("-n", "--num_slice", type=int, default=1)
+    parser.add_argument("-w", "--num_workers", type=int, default=1)
+    parser.add_argument("-s", "--num_slice", type=int, default=1)
     args = parser.parse_args()
 
     input_files = list(Path(args.h5_dir).glob("*.h5"))
