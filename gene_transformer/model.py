80b631f99b911512385d4fa8a1d3210f0f40d378
Kyle Hippe <kyle.hippe@gmail.com>
2022-09-14 09:45:38 -0500

    Trying shardid init

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -60,7 +60,7 @@ class DNATransformer(pl.LightningModule):
 
         # loads from a json file like this: https://huggingface.co/google/reformer-enwik8/blob/main/config.json
         self.base_config = AutoConfig.from_pretrained(self.cfg.model_config_json)
-        self.model = AutoModelForCausalLM.from_config(self.base_config)
+        # self.model = AutoModelForCausalLM.from_config(self.base_config)
 
     def configure_sharded_model(self):
         self.model = AutoModelForCausalLM.from_config(self.base_config)
