2cf87a3a28fb349404b0f6a9a26ed4825f5629c8
braceal <braceal@umich.edu>
2022-11-11 22:13:58 -0500

    rm old code

--- genslm/seq_utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def get_gc(seq: np.ndarray) -> float:
-    """Returns percentage GC content given sequence as a numpy array"""
-    g_content = np.sum(seq == "G")
-    c_content = np.sum(seq == "C")
-    return (g_content + c_content) / len(seq)
