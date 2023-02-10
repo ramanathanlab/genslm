8085cccdee046d466edd1139e33cb4e799f9f1ee
Kyle Hippe <kyle.hippe@gmail.com>
2023-02-10 12:23:05 -0600

    Double checking sequence lengths

--- genslm/cmdline/inference.py
+++ genslm/cmdline/inference.py
@@ -210,10 +210,9 @@ class OutputsCallback(Callback):
 
         if self.output_logits:
             logits = outputs.logits.detach().cpu().numpy()
-            print(f"{logits.shape=}, {seq_lens.shape=}, {fasta_inds.shape=}")
             for logit, seq_len, fasta_ind in zip(logits, seq_lens, fasta_inds):
                 self.h5logit_file["logits"].create_dataset(
-                    f"{fasta_ind}", data=logit[1 : seq_len + 1], **self.h5_kwargs,
+                    f"{fasta_ind}", data=logit[:seq_len], **self.h5_kwargs,
                 )
 
         if self.output_embeddings:
@@ -240,7 +239,7 @@ class OutputsCallback(Callback):
                 embed = embeddings.detach().cpu().numpy()
                 for emb, seq_len, fasta_ind in zip(embed, seq_lens, fasta_inds):
                     h5_file["embeddings"].create_dataset(
-                        f"{fasta_ind}", data=emb[1 : seq_len + 1], **self.h5_kwargs,
+                        f"{fasta_ind}", data=emb[:seq_len], **self.h5_kwargs,
                     )
 
                 h5_file.flush()
