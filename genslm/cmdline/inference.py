89ecf4edf60baa871e70fde9db0c11deb3665a28
Kyle Hippe <kyle.hippe@gmail.com>
2023-02-10 12:54:30 -0600

    Verifying that the dataset (tokenizer) is and forever shall be correct

--- genslm/cmdline/inference.py
+++ genslm/cmdline/inference.py
@@ -169,7 +169,6 @@ class OutputsCallback(Callback):
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
         # Plus one for embedding layer
-        # TODO Alex please check this :)
         num_hidden_layers = pl_module.model.model.config.num_hidden_layers + 1
 
         if self.layer_lb is not None and self.layer_lb < 0:
