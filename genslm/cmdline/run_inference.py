15b39ba9f9c3315c4fbae8f32c962c2efeb06c6f
Kyle Hippe <kyle.hippe@gmail.com>
2023-02-27 15:56:24 -0600

    testing logits hashes

--- genslm/cmdline/run_inference.py
+++ genslm/cmdline/run_inference.py
@@ -392,7 +392,7 @@ class OutputsCallback(Callback):
             self.h5logit_file.create_dataset(
                 "fasta-indices", data=self.indices, **self.h5_kwargs
             )
-            print(self.na_hashes)
+            print(self.na_hashes, flush=True)
             self.h5logit_file.create_dataset(
                 "na-hashes", data=self.na_hashes, **self.h5_kwargs
             )
